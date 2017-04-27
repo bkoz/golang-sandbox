c0405e0fd785efbc029e5be64cd4c7ead77c232d
Bob Kozdemba <bkozdemba@gmail.com>
2017-04-27 00:55:11 -0500

    cleaned up

--- frvector/frvector.go
+++ frvector/frvector.go
@@ -24,21 +24,21 @@ func (v *Vector) Get() Vector {
 	return *v
 }
 
-// GetOrigin - Returns the origin of the vector.
+// GetOrigin - Returns the i'th component of the vector origin.
 func (v *Vector) GetOrigin(i int) float64 {
 	return v.o[i]
 }
 
-// GetDirection - Returns the origin of the vector.
+// GetDirection - Returns the i'th component of the vector direction.
 func (v *Vector) GetDirection(i int) float64 {
 	return v.d[i]
 }
 
-// Normalize - Normalize the direction of the vector.
+// Normalize - Normalize the i'th component of the vector direction.
 func (v *Vector) Normalize() float64 {
 	var d = math.Sqrt(v.d[0]*v.d[0] + v.d[1]*v.d[1] + v.d[2]*v.d[2])
-	v.d[0] = v.d[0] / d
-	v.d[1] = v.d[1] / d
-	v.d[2] = v.d[2] / d
+	v.d[0] /= d
+	v.d[1] /= d
+	v.d[2] /= d
 	return d
 }
