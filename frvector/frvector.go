0786e2579d4edc8844ea840cf36f92ab6e3bd74d
Bob Kozdemba <bkozdemba@gmail.com>
2017-04-29 21:22:03 -0500

    Changed Vector to Vec3

--- frvector/frvector.go
+++ frvector/frvector.go
@@ -1,51 +1,42 @@
-// Package frvector - A collection of vector routines.
-package frvector
+// Package frVector - A collection of Vector routines.
+package frVector
 
 import "math"
 
-// Vector - It must be encapsulated in a struct to hide from the caller.
-type Vector struct {
-	o [3]float64
-	d [3]float64
+// Vec3 - It must be encapsulated in a struct to hide from the caller.
+type Vec3 struct {
+	c [3]float64
 }
 
-// Set - Sets a vector given 3 floats.
-func (v *Vector) Set(ox, oy, oz, dx, dy, dz float64) {
-	v.o[0] = ox
-	v.o[1] = oy
-	v.o[2] = oz
-	v.d[0] = dx
-	v.d[1] = dy
-	v.d[2] = dz
+// Set - Sets a Vec3 given 3 floats.
+func (v *Vec3) Set(x, y, z float64) {
+	v.c[0] = x
+	v.c[1] = y
+	v.c[2] = z
 }
 
-// Get - Returns the vector.
-func (v *Vector) Get() Vector {
+// Get - Returns the Vec3.
+func (v *Vec3) Get() Vec3 {
 	return *v
 }
 
-// GetOrigin - Returns the i'th component of the vector origin.
-func (v *Vector) GetOrigin(i int) float64 {
-	return v.o[i]
+// GetOrigin - Returns the i'th component of the Vec3 origin.
+func (v *Vec3) GetComponent(i int) float64 {
+	return v.c[i]
 }
 
-// GetDirection - Returns the i'th component of the vector direction.
-func (v *Vector) GetDirection(i int) float64 {
-	return v.d[i]
-}
-
-// Normalize - Normalize the i'th component of the vector direction.
-func (v *Vector) Normalize() float64 {
-	var d = math.Sqrt(v.d[0]*v.d[0] + v.d[1]*v.d[1] + v.d[2]*v.d[2])
-	v.d[0] /= d
-	v.d[1] /= d
-	v.d[2] /= d
+// Normalize - Normalize the i'th component of the Vec3 direction.
+func (v *Vec3) Normalize() float64 {
+	var d = math.Sqrt(v.c[0]*v.c[0] + v.c[1]*v.c[1] + v.c[2]*v.c[2])
+	v.c[0] /= d
+	v.c[1] /= d
+	v.c[2] /= d
 	return d
 }
 
 // Scale - Scale the origin by 's' along the direction.
-func (v *Vector) Scale(s float64) {
-	v.o[0] = v.d[0] * s
-	v.o[1] = v.d[1] * s
-	v.o[2] = v.d[2] * s
+func (v *Vec3) Scale(s float64) {
+	v.c[0] = v.c[0] * s
+	v.c[1] = v.c[1] * s
+	v.c[2] = v.c[2] * s
 }
--- main/main.go
+++ main/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "github.com/bkoz/golang-sandbox/frvector"
+import "github.com/bkoz/golang-sandbox/frVector"
 
 // Main - Testing local packages and built-in golang functions.
 func main() {
@@ -10,17 +10,15 @@ func main() {
 		b
 	)
 
-	var z frvector.Vector
-
-	z.Set(0, 0, 0, 0.707, 1.414, 3.52)
+	var z frVector.Vec3
+	z.Set(0.707, 1.414, 3.52)
 	var localvec = z.Get()
 
 	fmt.Println("rev. 1.1")
 	fmt.Println("localvec = ", localvec)
 	z.Scale(2)
 	fmt.Println("scaled = ", z.Get())
-	fmt.Println("localvec origin [1] = ", localvec.GetOrigin(1))
-	fmt.Println("localvec dir [1] = ", localvec.GetDirection(1))
+	fmt.Println("localvec[1] = ", localvec.GetComponent(1))
 	fmt.Println("distance = ", z.Normalize())
 	localvec = z.Get()
 	fmt.Println("normalized dir [1] = ", localvec)
